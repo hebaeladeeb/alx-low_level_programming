1efc1fdcca47b5dfd4363c4f64cca0c9c411d6e1
hebaeladeeb <hebaeladeeb@gmail.com>
2023-04-20 18:14:50 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+# include <stdio.h>
+/**
+ * *main -Aprogram that prints a line with puts function
+ * * return : 0 (success)
+ */
+int main (void)
+ {
+	 puts ("\"Programming is like building a multilingual puzzle")
+		 return (0);
+ }
+	
