0782703711903cafc5d9e9c5dcf92344f1a8b5cc
hebaeladeeb <hebaeladeeb@gmail.com>
2023-04-26 17:11:06 -0700

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ for (i = n; i <= 98; i++)
 if (i != 98)
 printf("%d ", i);
 else if (i == 98)
-printf("%d\n ", i);
+printf("%d\n", i);
 }
 }
 else if (n >= 98)
@@ -26,7 +26,7 @@ for (j = n; j >= 98; j--)
 if (j != 98)
 printf("%d ", j);
 else if (j == 98)
-printf("%d\n ", j);
+printf("%d\n", j);
 }
 }
 }
