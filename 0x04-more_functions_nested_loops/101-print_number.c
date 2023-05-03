73dcb3640f294b2416f4934309766d757647fd3f
hebaeladeeb <hebaeladeeb@gmail.com>
2023-05-03 15:38:24 -0700

    task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,7 +11,7 @@ unsigned int n1;
 if (n < 0)
 {
 n1 = -n;
-putchar('-');
+_putchar('-');
 }
 else
 {
@@ -23,5 +23,5 @@ if (n1 / 10)
 print_number(n1 / 10);
 }
 
-putchar((n1 % 10) + '0');
+_putchar((n1 % 10) + '0');
 }
