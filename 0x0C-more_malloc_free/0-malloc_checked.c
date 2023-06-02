3b163af2a46aa0f0acccb8571fe792fe85129a38
hebaeladeeb <hebaeladeeb@gmail.com>
2023-06-02 01:37:56 -0700

    task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,13 @@
 /**
 *malloc_checked - allocates memory using malloc
 * @b: number of bytes to allocate
-* Return: a pointer to the allocated memory		
+* Return: a pointer to the allocated memory
 */
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
-ptr = malloc(b);		
+ptr = malloc(b);
 if (ptr == NULL)
 exit(98);
-return (ptr);		
+return (ptr);
 }
